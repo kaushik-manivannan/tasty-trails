ec11bfa005c94bd0d59c4aefb18623ddf885b5ba
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-11-21 11:34:49 -0500

    Add Code Documentation for index.js in routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,9 @@
+/**
+ * @fileoverview Registers the Post API routes for the Express application.
+ */
+
 import postRouter from "../routes/post-routes.js"
 
 export default (app) => {
-    app.use("/posts", postRouter);
+    app.use("/posts", postRouter); // Registers the Post API routes under the "/posts" endpoint.
 }
