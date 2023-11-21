8c0b540e19026b2a249fc4923cdce29127e0fb71
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-11-20 21:18:09 -0500

    Add Code Documentation for post.js

--- app/models/post.js
+++ app/models/post.js
@@ -1,7 +1,14 @@
+/**
+ * @module models/post
+ * @description Defines the Post Model Schema for interacting with the 'posts' collection in MongoDB.
+ */
+
 import mongoose from "mongoose";
 
+// Create a Schema object from mongoose.
 const Schema = mongoose.Schema;
 
+// Represents the structure of a post in the 'posts' collection.
 const PostSchema = new Schema({
     userId: {
         type: String,
@@ -32,6 +39,8 @@ const PostSchema = new Schema({
     versionKey: false
 });
 
+// Create a Mongoose Model for the 'posts' collection
 const PostModel = mongoose.model('Post', PostSchema);
 
+// Export the Post Model Schema for external use
 export default PostModel;
