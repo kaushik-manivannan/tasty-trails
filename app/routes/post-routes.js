72636f9ee54fce6f548cb2dafd17422e7ca76551
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-11-21 11:30:54 -0500

    Add Code Documentation for post-routes.js

--- app/routes/post-routes.js
+++ app/routes/post-routes.js
@@ -1,12 +1,17 @@
+/**
+ * @fileoverview Defines the Express router for handling Post API endpoints.
+ */
+
 import express from "express";
 import * as postController from "../controllers/post-controller.js"
 
 const router = express.Router();
 
-router.get('/', postController.getAllPosts);
-router.post('/', postController.createPost);
-router.get('/:postId', postController.getPostById);
-router.put('/:postId', postController.updatePost);
-router.delete('/:postId', postController.deletePost);
+
+router.get('/', postController.getAllPosts); // Route for retrieving all posts
+router.post('/', postController.createPost); // Route for creating a new post
+router.get('/:postId', postController.getPostById); // Route for retrieving details of a specific post by its ID.
+router.put('/:postId', postController.updatePost); // Route for updating details of a specific post by its ID.
+router.delete('/:postId', postController.deletePost); // Route for deleting a specific post by its ID.
 
 export default router;
