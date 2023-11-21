a0dd9358bfb64eb230bef5ed9ecf7e88fda00a91
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-11-21 16:47:15 -0500

    Added user routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,9 @@
  */
 
 import postRouter from "../routes/post-routes.js"
+import userRouter from "../routes/user-routes.js"
 
 export default (app) => {
     app.use("/posts", postRouter); // Registers the Post API routes under the "/posts" endpoint.
+    app.use("/users", userRouter); // Registers the User API routes under the "/user" endpoint.
 }
