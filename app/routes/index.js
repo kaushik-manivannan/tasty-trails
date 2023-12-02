1d3dfa0606441339fe55d0fe6929805a24b10370
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-01 22:44:49 -0500

    Fixed Bugs

---
+++ app/routes/index.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Registers the Post API routes for the Express application.
+ */
+
+import postRouter from "../routes/post-routes.js"
+import commentRouter from "../routes/comment-routes.js"
+import communityRouter from "../routes/community-routes.js"
+import userRouter from "../routes/user-routes.js"
+
+export default (app) => {
+    app.use("/posts", postRouter); // Registers the Post API routes under the "/posts" endpoint.
+    app.use("/comments", commentRouter); // Registers the Comment API routes under the "/comments" endpoint.
+    app.use("/communities", communityRouter); // Registers the Community API routes under the "/communities" endpoint.
+    app.use("/users", userRouter); // Registers the User API routes under the "/users" endpoint.
+}
