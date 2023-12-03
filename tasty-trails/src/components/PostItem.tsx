b6fe06bd0e796560a39c7e5372064220a132a7ef
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-02 19:51:21 -0500

    Add Post Components

--- tasty-trails/src/components/PostItem.tsx
+++ tasty-trails/src/components/PostItem.tsx
@@ -1,1 +1,33 @@
 import React from 'react';
+
+interface Post {
+  _id: {
+    $oid: string;
+  };
+  postId: string;
+  userId: string;
+  image: string;
+  description: string;
+  location: string;
+  dateTime: string;
+  availabilityStatus: string;
+}
+
+interface PostItemProps {
+  post: Post;
+}
+
+const PostItem: React.FC<PostItemProps> = ({ post }) => {
+  const { _id, postId, userId, image, description, location, dateTime, availabilityStatus } = post;
+
+  return (
+    <>
+      <h2>{description}</h2>
+      <p>User ID: {userId}</p>
+      <p>Location: {location}</p>
+      {/* Render other details */}
+    </>
+  );
+};
+
+export default PostItem;
--- tasty-trails/src/containers/PostItemContainer.tsx
+++ tasty-trails/src/containers/PostItemContainer.tsx
@@ -1,1 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import PostItem from '../components/PostItem.tsx';
+
+const PostItemContainer: React.FC<{ postId: string }> = ({ postId }) => {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/posts/${postId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPost(data);
+      })
+      .catch((error) => {
+        console.error(`Error fetching post with ID ${postId}:`, error);
+      });
+  }, [postId]);
+
+  return (
+    <div>
+      {post && <PostItem post={post} />}
+    </div>
+  );
+};
+
+export default PostItemContainer;
