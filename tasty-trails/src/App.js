3ad74c33e1190306a1872313ada7041c4b3d8d9c
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-02 19:47:41 -0500

    Initialize React Router

--- tasty-trails/src/App.js
+++ tasty-trails/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import PostListContainer from './containers/PostListContainer.tsx';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/posts' element={ <PostListContainer /> }>
+    //Add routes
+  </Route>
+));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={ router } />
   );
 }
 
