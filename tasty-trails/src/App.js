7b1dfc82f0e5d064d41f184d705866551cc85270
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2023-12-04 00:09:59 -0500

    Modify routes for comment

--- tasty-trails/src/App.js
+++ tasty-trails/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import PostListContainer from './containers/PostListContainer.tsx';
+import CommentListContainer from './containers/CommentListContainer.tsx';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/posts' element={ <PostListContainer /> }>
-    //Add routes
-  </Route>
-));
+const router = createBrowserRouter(createRoutesFromElements([
+  <Route path='/posts' element={ <PostListContainer /> }/>,
+  <Route path='/comments' element={ <CommentListContainer /> }/>
+]));
 
 function App() {
   return (
