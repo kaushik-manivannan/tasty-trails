18374de209e410bdb4dc4fc79e690f5de4ff8df1
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-03 19:20:04 -0500

    Added Signup Route

--- tasty-trails/src/App.js
+++ tasty-trails/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import PostListContainer from './containers/PostListContainer.tsx';
+import SignupFormContainer from './containers/SignUpFormContainer.tsx';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/posts' element={ <PostListContainer /> }>
-    //Add routes
-  </Route>
-));
+const router = createBrowserRouter(createRoutesFromElements([
+  <Route path='/posts' element={ <PostListContainer /> }/>,
+  <Route path='/signUp' element={ <SignupFormContainer/> }/>
+]));
 
 function App() {
   return (
       <RouterProvider router={ router } />
+     
   );
 }
 
