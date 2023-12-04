031ee0aa0f9d0455ed9a372a4cfc8582e7020fb0
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-03 19:05:26 -0500

    [feat]: update router for communites

--- tasty-trails/src/App.js
+++ tasty-trails/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import PostListContainer from './containers/PostListContainer.tsx';
+import CommunityListContainer from './containers/CommunityListContainer.tsx';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/posts' element={ <PostListContainer /> }>
-    //Add routes
-  </Route>
-));
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path='/posts' element={ <PostListContainer /> }>
+      //Add routes
+    </Route>,
+    <Route path='/communites' element={ <CommunityListContainer /> }>
+      //Add routes
+    </Route>
+  ])
+);
 
 function App() {
   return (
