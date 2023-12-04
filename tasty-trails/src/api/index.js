3f54b3e73acf9cddf639a6871ad65763dfb6d347
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-03 19:17:21 -0500

    Added create user api in axios

--- tasty-trails/src/api/index.js
+++ tasty-trails/src/api/index.js
@@ -4,3 +4,8 @@ const API = axios.create({ baseURL: 'http://localhost:8080' });
 
 
 export const fetchPosts = () => API.get('/posts');
+
+
+//User API's
+export const createUser = (data) => API.post('/users',data);
+
