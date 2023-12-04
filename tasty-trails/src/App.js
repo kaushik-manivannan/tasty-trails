dfce59fffd09ec775df9a073378bcbfdbb885679
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2023-12-04 00:19:07 -0500

    Merge conflict fix

--- tasty-trails/src/App.js
+++ tasty-trails/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import PostListContainer from './containers/PostListContainer.tsx';
 import CommentListContainer from './containers/CommentListContainer.tsx';
+import CommunityListContainer from './containers/CommunityListContainer.tsx';
 
 const router = createBrowserRouter(createRoutesFromElements([
   <Route path='/posts' element={ <PostListContainer /> }/>,
