4275911c3cef07c5c01fb8403aad44af401b3e55
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-06 13:18:38 -0500

    Create Interfaces for Post

---
+++ tasty-trails-client/src/interfaces/post-interfaces.tsx
@@ -0,0 +1,24 @@
+export interface Post {
+    _id: {
+      $oid: string;
+    };
+    userId: string;
+    image: string;
+    description: string;
+    location: string;
+    availabilityStatus: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface PostItemProps {
+    post: Post;
+}
+
+export interface PostListProps {
+    posts: Post[];
+}
+
+
+
+
