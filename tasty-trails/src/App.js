a4b1f763674d07c13899ea79f46f1fd1c7b90ea0
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-07 01:41:34 -0500

    Added login route and added store provider

--- tasty-trails/src/App.js
+++ tasty-trails/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './auth/store.js';
 import PostListContainer from './containers/PostListContainer.tsx';
 import SignupFormContainer from './containers/SignUpFormContainer.tsx';
+import LoginFormContainer from './containers/LoginFormContainer.tsx';
 
 const router = createBrowserRouter(createRoutesFromElements([
   <Route path='/posts' element={ <PostListContainer /> }/>,
-  <Route path='/signUp' element={ <SignupFormContainer/> }/>
+  <Route path='/signUp' element={ <SignupFormContainer/> }/>,
+  <Route path='/login' element = {<LoginFormContainer/>}/>
 ]));
 
 function App() {
   return (
+    <Provider store={store}>
       <RouterProvider router={ router } />
+    </Provider>
      
   );
 }
