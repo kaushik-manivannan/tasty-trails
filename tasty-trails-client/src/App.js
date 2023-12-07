3279ae8b7f145ee10b3bdd1c853658968d6b4ce9
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-06 21:14:24 -0500

    Add View in Routes

--- tasty-trails-client/src/App.js
+++ tasty-trails-client/src/App.js
@@ -1,13 +1,13 @@
 import './App.scss';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import PostListContainer from './containers/PostListContainer.tsx';
-import PostDetailsContainer from './containers/PostDetailsContainer.tsx';
 import CommentListContainer from './containers/CommentListContainer.tsx';
 import CommunityListContainer from './containers/CommunityListContainer.tsx';
+import LandingPage from './views/LandingPage/LandingPage.tsx';
+import PostDetailsPage from './views/PostDetailsPage/PostDetailsPage.tsx';
 
 const router = createBrowserRouter(createRoutesFromElements([
-  <Route path='/posts' element={ <PostListContainer /> }/>,
-  <Route path='/posts/:postId' element={ <PostDetailsContainer /> } />,
+  <Route path='/posts' element={ <LandingPage /> }/>,
+  <Route path='/posts/:postId' element={ <PostDetailsPage /> } />,
   <Route path='/comments' element={ <CommentListContainer /> }/>,
   <Route path='/communites' element={ <CommunityListContainer /> }/>
 ]));
