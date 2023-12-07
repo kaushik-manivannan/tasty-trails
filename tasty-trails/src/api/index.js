aa72b5e02a0075e6d9c23fa78542bc1444ff7b19
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-07 01:40:28 -0500

    Added request interceptor to added Bearer token

--- tasty-trails/src/api/index.js
+++ tasty-trails/src/api/index.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
+import store from '../auth/store.js';
+
 
 const API = axios.create({ baseURL: 'http://localhost:8080' });
+
+API.interceptors.request.use((req) => {
+
+     const { token } = store.getState().auth;
+
+  
+      if (token) {
+        req.headers.Authorization = `Bearer ${token}`;
+      }
+  
+      return req;
+    
+});
 
 
 export const fetchPosts = () => API.get('/posts');
@@ -8,4 +23,5 @@ export const fetchPosts = () => API.get('/posts');
 
 //User API's
 export const createUser = (data) => API.post('/users',data);
+export const loginUser = (data) => API.post('/users/login',data);
 
