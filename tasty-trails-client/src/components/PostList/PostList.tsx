a28b60c440df155aab7efc9f76b7fa68bcd28b3a
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-07 12:15:07 -0500

    Add Styles for Logout Button

--- tasty-trails-client/src/components/PostList/PostList.tsx
+++ tasty-trails-client/src/components/PostList/PostList.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import PostItem from '../PostItem/PostItem.tsx';
 import styles from './PostList.module.scss';
 import { PostListProps } from '../../interfaces/post-interfaces.tsx';
-import LogoutButton from '../LogoutButton.tsx';
 
 const PostList: React.FC<PostListProps> = ({ posts }) => {
 
   return (
     <div className={styles.postFeed}>
-      <LogoutButton/>
       <h2 className={styles.heading}>Your Posts</h2>
       {posts.slice(0).reverse().map((post, idx) => {
         return <PostItem key={`post-${idx + 1}`} post={post} />;
