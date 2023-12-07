6f8f855316ace30be7ed7c1c8e5810f211aafa5a
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-06 21:12:44 -0500

    Add Back Button in PostDetailsComponent

--- tasty-trails-client/src/components/PostDetails/PostDetails.tsx
+++ tasty-trails-client/src/components/PostDetails/PostDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PostItemProps } from '../../interfaces/post-interfaces';
 import styles from './PostDetails.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const PostDetails: React.FC<PostItemProps> = ({ post }) => {
 
@@ -15,20 +16,27 @@ const PostDetails: React.FC<PostItemProps> = ({ post }) => {
   };
   const formattedDate = date.toLocaleDateString('en-US', options);
 
+  const navigate = useNavigate();
+
   return (
-      <div className={styles.card}>
-          <img src={post.image} className={styles.image}/>
-          <h3 className={styles.heading}>{post.description}</h3>
-          <div className={styles.locationAndStatus}>
-            <div className={styles.location}>
-              <img src={`${process.env.PUBLIC_URL}/assets/location.svg`} alt="Location" className={styles.locationIcon}/>
-              <p className={styles.locationText}>{post.location}</p>
+      <div className={styles.postDetails}>
+        <button onClick={() => navigate(-1)} className={styles.backButton}>
+          <img src={`${process.env.PUBLIC_URL}/assets/back-arrow.svg`} alt="Back Button"/>
+        </button>
+        <div className={styles.card}>
+            <img src={post.image} className={styles.image}/>
+            <h3 className={styles.heading}>{post.description}</h3>
+            <div className={styles.locationAndStatus}>
+              <div className={styles.location}>
+                <img src={`${process.env.PUBLIC_URL}/assets/location.svg`} alt="Location" className={styles.locationIcon}/>
+                <p className={styles.locationText}>{post.location}</p>
+              </div>
+              <div className={styles.availabilityStatus}>
+                <p>Status: {post.availabilityStatus}</p>
+              </div>
             </div>
-            <div className={styles.availabilityStatus}>
-              <p>Status: {post.availabilityStatus}</p>
-            </div>
-          </div>
-          <p className={styles.date}>Posted on: {formattedDate}</p>
+            <p className={styles.date}>Posted on: {formattedDate}</p>
+        </div>
       </div>
   );
 };
