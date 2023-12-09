5238fc43b4d508a2830b43b5210bd2d393a8e174
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-08 22:13:17 -0500

    Modified redux store as persisted store

--- tasty-trails-client/src/auth/store.js
+++ tasty-trails-client/src/auth/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from './authSlice.ts';
 
-const store = configureStore({
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+
+export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
 });
 
-export default store;
+export const persistor = persistStore(store);
