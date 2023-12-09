dde300107fa19c77277cd26ded94a1c654f31ac2
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-08 23:02:48 -0500

    Redux serializable error fix

--- tasty-trails-client/src/auth/store.js
+++ tasty-trails-client/src/auth/store.js
@@ -14,6 +14,10 @@ export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    })
 });
 
 export const persistor = persistStore(store);
