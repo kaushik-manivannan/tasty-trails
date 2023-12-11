7c37fd0599dc24e4e02555bb658fec4fa5ab75a6
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-11 15:06:58 -0500

    Modified Styles in PostItem

--- tasty-trails-client/src/components/PostItem/PostItem.tsx
+++ tasty-trails-client/src/components/PostItem/PostItem.tsx
@@ -7,7 +7,6 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
 
   const date = new Date(post.createdAt);
   const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
     month: 'short', 
     day: '2-digit',
     hour: 'numeric',
@@ -20,17 +19,16 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
     <Link to={`/posts/${post._id}`} className={styles.cardLink}>
       <div className={styles.card}>
         <img src={post.image} className={styles.image}/>
-        <h3 className={styles.heading}>{post.description}</h3>
-        <div className={styles.locationAndStatus}>
-          <div className={styles.location}>
-            <img src={`${process.env.PUBLIC_URL}/assets/location.svg`} alt="Location" className={styles.locationIcon}/>
-            <p className={styles.locationText}>{post.location}</p>
-          </div>
-          <div className={styles.availabilityStatus}>
-            <p>Status: {post.availabilityStatus}</p>
+        <div className={styles.content}>
+          <h3 className={styles.heading}>{post.description}</h3>
+          <div className={styles.locationAndDate}>
+            <div className={styles.location}>
+              <img src={`${process.env.PUBLIC_URL}/assets/location.svg`} alt="Location" className={styles.locationIcon}/>
+              <p className={styles.locationText}>{post.location}</p>
+            </div>
+            <p className={styles.date}>{formattedDate}</p>
           </div>
         </div>
-        <p className={styles.date}>Posted on: {formattedDate}</p>
       </div>
     </Link>
   );
