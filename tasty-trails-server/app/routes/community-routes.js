36166b09feae8b0f2e9a903463ead14edb640d95
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-11 01:40:24 -0500

    [UPDATE]: revert authorization

--- tasty-trails-server/app/routes/community-routes.js
+++ tasty-trails-server/app/routes/community-routes.js
@@ -8,9 +8,9 @@ import {protect as auth}  from  "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.use(auth).get("/", CommunityController.getCommunities); // Route for retrieving  communities
-router.use(auth).post("/", CommunityController.addNewCommunity); // Route for adding a new community
-router.use(auth).get("/:id", CommunityController.getCommunityById); // Route for geting a community by id
-router.use(auth).put("/:id", CommunityController.updateCommunity); // Route for updating a community
+router.get("/", CommunityController.getCommunities); // Route for retrieving  communities
+router.post("/", CommunityController.addNewCommunity); // Route for adding a new community
+router.get("/:id", CommunityController.getCommunityById); // Route for geting a community by id
+router.put("/:id", CommunityController.updateCommunity); // Route for updating a community
 
 export default router;
