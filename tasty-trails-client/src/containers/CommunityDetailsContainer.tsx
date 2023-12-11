329df583be07b848684a3a5a06296e49eaaaf45e
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-11 03:55:56 -0500

    [UPDATE]: update axios function call insted of fetch

--- tasty-trails-client/src/containers/CommunityDetailsContainer.tsx
+++ tasty-trails-client/src/containers/CommunityDetailsContainer.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import CommunityDetails from '../components/CommunityDetails/CommunityDetails.tsx';
 import { getAllCommunityDetailsById } from '../api/index.js';
 import { useSelector } from 'react-redux';
+import {updateCommunityById} from '../api/index.js';
+
 
 const CommunityDetailsContainer:React.FC =()=>{
     const [community, setCommunity] = useState(null);
@@ -10,11 +12,22 @@ const CommunityDetailsContainer:React.FC =()=>{
     const [isEditable, setIsEditable] = useState(false);
     const {communityId} = useParams();
     const userId = useSelector((state:any) => state.auth.userId);
+    const updateCommunity = async (payload:any) => {
+      try {
+        const response = await updateCommunityById(communityId,payload);
+        if(response.status!== 200){
+          throw new Error(`Error occured while fetching communities with response : ${response.data}`);
+        }
+        setCommunity(response.data);
+      } catch (error) {
+        throw new Error(`Error occured while fetching communities Error`);
+      }
+    };
     const fetchPostByCommunityId = async () => {
         try {
             const response = await getAllCommunityDetailsById(communityId);
             if(response.status !== 200){
-            throw new Error("Failed to fetch posts.");
+            throw new Error("Failed to fetch get the community details");
             }
             setCommunity(response.data.community);
             setPostList(response.data.posts);
@@ -37,6 +50,6 @@ const CommunityDetailsContainer:React.FC =()=>{
       
         fetchData();
       }, [communityId]); 
-    return (<>{community && <CommunityDetails community={community} postList ={PostList} isEditable={isEditable}/>}</>);
+    return (<>{community && <CommunityDetails community={community} postList ={PostList} isEditable={isEditable} updateCommunityById={updateCommunity}/>}</>);
 }
 export default CommunityDetailsContainer;
