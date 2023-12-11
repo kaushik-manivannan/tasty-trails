63d6d67fc013e14cf93d2f953c08ff0bd0d7e114
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-11 03:52:26 -0500

    [UPDATE]: restructure html for editing the community

--- tasty-trails-client/src/components/CommunityDetails/CommunityDetails.tsx
+++ tasty-trails-client/src/components/CommunityDetails/CommunityDetails.tsx
@@ -1,11 +1,13 @@
-import React ,{useState} from "react";
+import React ,{useState,useEffect} from "react";
 import PostList from '../PostList/PostList';
 import {CommunityDetailsProps} from '../../interfaces/community-interfaces';
 import styles from './CommunityDetails.module.scss';
 
-const CommunityDetails:React.FC<CommunityDetailsProps> = ({community,postList,isEditable})=>{
+const CommunityDetails:React.FC<CommunityDetailsProps> = ({community,postList,isEditable,updateCommunityById})=>{
     const [editedCommunityName, setEditedCommunityName] = useState(community.communityName);
     const [editedCommunityDescription, setEditedCommunityDescription] = useState(community.description);
+    const [updateMessage, setUpdateMessage] = useState("");
+    const [isEditClicked, setIsEditClicked] = useState(false);
     const handleCommunityNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEditedCommunityName(event.target.value);
     };
@@ -13,41 +15,49 @@ const CommunityDetails:React.FC<CommunityDetailsProps> = ({community,postList,is
     const handleCommunityDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEditedCommunityDescription(event.target.value);
     };
+
     return(
         <div className={styles.parentContainer}>
             <div className={styles.communityDetailsContainer}>
                 <h2 className={styles.communityDetailsHeading}>Community Details</h2>
-                {isEditable ? (
+                {updateMessage && (
+                    <div className={styles.updateMessage}>
+                        {updateMessage}
+                    </div>
+                )}
+                {(isEditable&& isEditClicked)?(<>
                 <div className={styles.inputContainer}>
                     <label htmlFor="communityName" className={styles.inputLabel}>Community Name</label>
-                    <input id="communityName" type="text" value={editedCommunityName} onChange={handleCommunityNameChange} className={styles.input}/>
-                   
+                    <input id="communityName" type="text" value={editedCommunityName} onChange={handleCommunityNameChange} className={styles.input}/>   
                 </div>
-                ) : (
-                <div className={styles.inputContainer}>
-                    <strong>Community Name:</strong> {editedCommunityName}
-                </div>
-                )}
-
-                {isEditable ? (
                 <div className={styles.inputContainer}>
                     <label htmlFor="communityDescription" className={styles.inputLabel}>Community Description</label>
                     <input id="communityDescription" type="text" value={editedCommunityDescription} onChange={handleCommunityDescriptionChange} className={styles.input}/>
                 </div>
-                ) : (
+                </>
+                ):(<>
+                <div className={styles.inputContainer}>
+                    <strong>Community Name:</strong> {editedCommunityName}
+                </div>
                 <div className={styles.inputContainer}>
                     <strong>Community Description:</strong> {editedCommunityDescription}
                 </div>
+                </>
                 )}
                 <div className={styles.inputContainer}>
                     <strong className={styles.inputLabel}>Community Members</strong>
                     <p className={styles.memberCount}>{community?community.members.length:""}</p>
                 </div>
-
                 {isEditable && (
-                    <div>
-                        <button className={styles.updateButton}>Update</button>
-                    </div>
+                    isEditClicked ? (
+                        <div>
+                            <button className={styles.updateButton} onClick={updateCommunity}>Update</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button className={styles.updateButton} onClick={edit}>Edit</button>
+                        </div>
+                    )
                 )}
             </div>
             <div className={styles.postListContainer}>
