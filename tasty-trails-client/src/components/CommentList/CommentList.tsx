4cbdd38fa2860fcc5175e981c7c7ff41aefdf1bb
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-11 11:33:28 -0500

    Fix Sidebar in CommunityListPage

--- tasty-trails-client/src/components/CommentList/CommentList.tsx
+++ tasty-trails-client/src/components/CommentList/CommentList.tsx
@@ -4,17 +4,17 @@ import CommentItemContainer from '../../containers/CommentItemContainer.tsx';
  
 const CommentList: React.FC<CommentListProps & { onEdit: (commentId: { $oid: string }, editedComment: string) => void; onDelete: (commentId: { $oid: string }) => void }> = ({ comments, onEdit, onDelete }) => {
   return (
-<div>
+    <div>
       {comments.map((comment, idx) => (
-<CommentItemContainer
-          key={`comment-${comment._id}`}
-          commentValue={comment}
-          onEdit={(editedComment) => onEdit(comment._id, editedComment)}
-          onDelete={() => onDelete(comment._id)}
-        />
+      <CommentItemContainer
+        key={`comment-${comment._id}`}
+        commentValue={comment}
+        onEdit={(editedComment) => onEdit(comment._id, editedComment)}
+        onDelete={() => onDelete(comment._id)}
+      />
       ))}
-<p>Total Comments: {comments.length}</p>
-</div>
+      <p>Total Comments: {comments.length}</p>
+    </div>
   );
 };
  
