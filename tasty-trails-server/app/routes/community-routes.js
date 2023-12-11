6308260529fbf8f798e157d9a732f7c510889560
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-11 00:03:25 -0500

    [ADD]:add authrization to the back end calls

--- tasty-trails-server/app/routes/community-routes.js
+++ tasty-trails-server/app/routes/community-routes.js
@@ -4,12 +4,13 @@
 
 import express from "express";
 import * as CommunityController from "../controllers/community-controller.js";
+import {protect as auth}  from  "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", CommunityController.getCommunities); // Route for retrieving  communities
-router.post("/", CommunityController.addNewCommunity); // Route for adding a new community
-router.get("/:id", CommunityController.getCommunityById); // Route for geting a community by id
-router.put("/:id", CommunityController.updateCommunity); // Route for updating a community
+router.use(auth).get("/", CommunityController.getCommunities); // Route for retrieving  communities
+router.use(auth).post("/", CommunityController.addNewCommunity); // Route for adding a new community
+router.use(auth).get("/:id", CommunityController.getCommunityById); // Route for geting a community by id
+router.use(auth).put("/:id", CommunityController.updateCommunity); // Route for updating a community
 
 export default router;
