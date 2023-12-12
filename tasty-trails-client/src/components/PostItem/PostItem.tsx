26006609aadedc05e39eee3cdbbe852e8aed83ed
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-12 05:58:50 -0500

    [ADD]: map link on the post item

--- tasty-trails-client/src/components/PostItem/PostItem.tsx
+++ tasty-trails-client/src/components/PostItem/PostItem.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { PostItemProps } from '../../interfaces/post-interfaces';
 import { Link } from 'react-router-dom';
 import styles from './PostItem.module.scss';
-
 const PostItem: React.FC<PostItemProps> = ({ post }) => {
-
+  const latitude = post.latitude;
+  const longitude = post.longitude;
+  const isValidLocation = latitude!==0 && longitude!=0;
+  const mapUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
   const date = new Date(post.createdAt);
   const options: Intl.DateTimeFormatOptions = {
     month: 'short', 
@@ -14,9 +16,14 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
     hour12: true
   };
   const formattedDate = date.toLocaleDateString('en-US', options);
-  
+  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (event.target instanceof HTMLAnchorElement) {
+      return;
+    }
+
+  };
   return (
-    <Link to={`/posts/${post._id}`} className={styles.cardLink}>
+    <Link to={`/posts/${post._id}`} className={styles.cardLink} onClick={handleLinkClick}>
       <div className={styles.card}>
         <img src={post.image} className={styles.image}/>
         <div className={styles.content}>
@@ -24,7 +31,15 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
           <div className={styles.locationAndDate}>
             <div className={styles.location}>
               <img src={`${process.env.PUBLIC_URL}/assets/location.svg`} alt="Location" className={styles.locationIcon}/>
-              <p className={styles.locationText}>{post.location}</p>
+              {isValidLocation?
+              (<a
+                href={mapUrl}
+                target="_blank"
+                rel="noopener noreferrer">
+                  <p className={styles.locationText}>{post.location}</p>
+              </a>):(
+                <p className={styles.locationText}>{post.location}</p>
+                )}
             </div>
             <p className={styles.date}>{formattedDate}</p>
           </div>
