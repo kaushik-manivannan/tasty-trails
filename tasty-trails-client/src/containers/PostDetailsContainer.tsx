74853e140a5f8b70bbad90386cbd40e8258a97e5
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-12 01:10:38 -0500

    [UPDATE]: UPDATE  post details container

--- tasty-trails-client/src/containers/PostDetailsContainer.tsx
+++ tasty-trails-client/src/containers/PostDetailsContainer.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { getPost } from '../api/index.js';
 import PostDetails from '../components/PostDetails/PostDetails.tsx';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import { PostFormData } from "../interfaces/post-interfaces";
 
 const PostDetailsContainer: React.FC = () => {
   const [post, setPost] = useState(null);
+  const [canModify,setCanModify] = useState(false);
   const { postId } = useParams();
-
+  const { register, handleSubmit, formState: { errors },getValues,setValue } = useForm<PostFormData>({
+    mode: 'onChange',
+  });
+  const userId = useSelector((state:any) => state.auth.userId);
+  const handleDelete = () => {};
+  const handleEdit = () => {};   
   useEffect(() => {
     const fetchPostById = async () => {
       try {
@@ -15,6 +24,8 @@ const PostDetailsContainer: React.FC = () => {
           throw new Error("Failed to fetch post.");
         }
         setPost(response.data);
+        if(response.data.userId === userId)
+          setCanModify(true);
       } catch(error){
         console.log("Error fetching post: ", error);
       }
@@ -22,7 +33,7 @@ const PostDetailsContainer: React.FC = () => {
     fetchPostById();
   }, [postId]);
 
-  return <>{post && <PostDetails post={post} />}</>;
+  return <>{post && <PostDetails post={post} handleDelete={handleDelete} handleEdit={handleEdit} canModify={canModify} />}</>;
 };
 
 export default PostDetailsContainer;
