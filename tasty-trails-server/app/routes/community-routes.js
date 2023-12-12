0fda99723f709c49b81c70e0ae791d924a963a57
srivijaykalki <poosarla.s@northeastern.edu>
2023-12-12 03:05:39 -0500

    [UPDATE]: add auth for the community.

--- tasty-trails-server/app/routes/community-routes.js
+++ tasty-trails-server/app/routes/community-routes.js
@@ -7,7 +7,7 @@ import * as CommunityController from "../controllers/community-controller.js";
 import {protect as auth}  from  "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
+router.use(auth);
 router.get("/", CommunityController.getCommunities); // Route for retrieving  communities
 router.post("/", CommunityController.addNewCommunity); // Route for adding a new community
 router.get("/:id", CommunityController.getCommunityById); // Route for geting a community by id
