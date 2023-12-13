f6bc4f747b7e87c0719d2a9ad0fb20d0b8f6d37d
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-12 22:16:12 -0500

    Added comments

--- tasty-trails-server/app/controllers/google-auth-controller.js
+++ tasty-trails-server/app/controllers/google-auth-controller.js
@@ -1,15 +1,19 @@
 import passport from 'passport';
-import * as responses from "./response-handler.js";
 import { generateAuthToken, createUser } from '../services/user-service.js';
 
-// export const googleCallback = passport.authenticate('google', {
-//     successRedirect: 'http://localhost:3000/',
-//     failureRedirect: '/users/login/failed',
-// });
 
-
+/**
+ * Callback function for Google authentication.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void}
+ */
 export const googleCallback = (req, res, next) => {
     passport.authenticate('google', async (err, user) => {
+    
+      // Handle authentication errors or if the user is not present
       if (err || !user) {
         return res.redirect('/users/login/failed');
       }
@@ -17,9 +21,11 @@ export const googleCallback = (req, res, next) => {
       console.log('inside google callback');
       console.log(user);
       
+      // Create a user or retrieve existing user and get the result
       const result = await createUser(user);
       let token ;
 
+      //if the user is an existing user generate token
       if(result.error) {
         token = generateAuthToken(result.user);
       }
@@ -27,9 +33,6 @@ export const googleCallback = (req, res, next) => {
         token = result.token;
       }
   
-      res.cookie('userId', result.user._id, { httpOnly: true });
-      res.cookie('authToken', token, { httpOnly: true });
-      console.log('response'+res);
       res.redirect(`http://localhost:3000/google/oauth/success?userId=${result.user._id}&val=${token}`);
     })(req, res, next);
   };
