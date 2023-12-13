721bb74e45ebe933e9edddafccea240a882e4c10
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-12 22:15:37 -0500

    Modified email content

--- tasty-trails-server/app/middleware/email.js
+++ tasty-trails-server/app/middleware/email.js
@@ -3,8 +3,19 @@ import nodemailer from 'nodemailer';
 // Replace these values with your email and password
 const emailUser = 'psvkyear2023@gmail.com';
 const emailPass = 'jnyrosoktlbluqbf';
-const emailText = "Welcome to Tasty Trails – your passport to a world of culinary delights!\n\nAt Tasty Trails, we're on a mission to bring people together through the joy of sharing food. Whether you're a seasoned chef or a passionate home cook, our platform is designed to inspire and connect you with a community of like-minded food enthusiasts.\n\nHere's what Tasty Trails has to offer:\n\nLeftovers Sharing: Reduce food waste by sharing your delicious leftovers with others in your community. Your culinary creations could be a delightful surprise for someone looking for a tasty treat\n\nExclusive Communities: Join specialized communities where members share exclusive posts.\n\nTasty Trails is more than an app; it's a vibrant community where food connects us all.\n\nWe're excited to have you as part of Tasty Trails. Your culinary adventure starts now, and we can't wait to see the delicious experiences you share with the community.";
+const emailText = (userName) => `
+Dear ${userName},
 
+Greetings and welcome to Tasty Trails!
+
+We are thrilled to have you as a part of our growing culinary community. At Tasty Trails, we believe that food is not just about sustenance; it's an adventure, a journey of flavors, and a celebration of cultures. We're excited that you've chosen to embark on this delicious journey with us.
+
+Thank you for choosing Tasty Trails. We're honored to be a part of your culinary adventure. If you ever have questions, feedback, or simply want to share your kitchen triumphs, feel free to reach out to us. Happy cooking!
+
+To delicious discoveries,
+The Tasty Trails Team
+`;
+// Creating a nodemailer transporter using Gmail service
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -13,12 +24,21 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendEmail = async (to) => {
+
+/**
+ * Sends a welcome email to the specified email address.
+ *
+ * @param {string} to - The recipient's email address.
+ * @param {string} userName - The username of the recipient.
+ * @returns {Promise<string>} A promise that resolves with the response from the email server.
+ * @throws {Error} Throws an error if there is an issue sending the email.
+ */
+export const sendEmail = async (to, userName) => {
   const mailOptions = {
     from: emailUser,
     to,
     subject: 'Welcome to Tasty Trails - Let Your Culinary Adventures Begin!',
-    text: emailText,
+    text: emailText(userName),
   };
 
   try {
@@ -31,6 +51,6 @@ export const sendEmail = async (to) => {
   }
 }
 
-
+sendEmail("balajivasanrj@gmail.com", "Balaji");
 
 
