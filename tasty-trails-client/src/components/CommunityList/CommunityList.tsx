12736aac825e1cd98d3823bc9668bca04644c762
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-12 19:25:42 -0500

    Add Code Documentation for CommunityList

--- tasty-trails-client/src/components/CommunityList/CommunityList.tsx
+++ tasty-trails-client/src/components/CommunityList/CommunityList.tsx
@@ -6,38 +6,44 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 /**
+ * CommunityList component displays all communities fetched from the database.
  * 
- * This component is responsible for displaying all communities fetch from the database
- * @param communities List of communities
- * @returns 
+ * @param {CommunityListProps} communities - List of communities.
+ * @returns {React.FC} - React functional component.
  */
-const CommunityList : React.FC<CommunityListProps> = ({communities}) => {
+const CommunityList: React.FC<CommunityListProps> = ({ communities }) => {
 
     // Check if the communities array is empty
     const isEmpty = communities.length === 0;
+    
+    // Translation hook
     const { t } = useTranslation();
+
     return (
         <div className={styles.parentContainer}>
             <div className={styles.communityListWithHeading}>
+
                 <div className={styles.titleContainer}>
+                    {/* Heading and link to create a new community */}
                     <h1 className={styles.heading}>{t('Discover Communities')}</h1>
-                    {/*  Link to redirect for creating a new community*/}
                     <Link to="/new-community" className={styles.createButton}>
-                        Create Community
+                        {t('Create Community')}
                     </Link>
                 </div>
+
                 <div className={styles.communityListContainer}>
+                    {/* Display communities or a message if empty */}
                     {isEmpty ? (
                         <p className={styles.noCommunities}>{t('No Communities Found!')}</p>
                     ) : (
-                    <ul className={styles.communityList}>
-                        {communities.map((community, idx) => (
-                            <li className={styles.communityItem} key={`community-${idx + 1}`}>
-                                <CommunityItemContainer community={community} />
-                            </li>
-                        ))}
-                    </ul>
-                )}
+                        <ul className={styles.communityList}>
+                            {communities.map((community, idx) => (
+                                <li className={styles.communityItem} key={`community-${idx + 1}`}>
+                                    <CommunityItemContainer community={community} />
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
