c9c05868d20c408a5ac6267b90bea745484ea6cb
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-13 21:43:59 -0500

    Fix User Route Error

--- tasty-trails-server/app/routes/user-routes.js
+++ tasty-trails-server/app/routes/user-routes.js
@@ -12,12 +12,10 @@ const authRouter = express.Router();
 
 router.post('/', userController.createUser); // Route for creating a new user
 router.post('/login', userController.loginUser); // Route for user login
-router.route('/:userId')
 authRouter.use(auth).route('/:userId')
     .get(userController.getUserById) // Route for retrieving details of a specific user by its ID.
-    .get('/communities',userController.getUserCommunities) // Route for retrieving communites of a specific user's communities
     .put(userController.updateUser) // Route for updating details of a specific user by its ID.
     .delete(userController.deleteUser) // Route for deleting a specific user by its ID.
 
-
+    authRouter.get('/:userId/communities',userController.getUserCommunities) // Route for retrieving communites of a specific user's communities
 export default { router, authRouter};
