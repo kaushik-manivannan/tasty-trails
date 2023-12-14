05555de514cda04fbdbc9d3f3bbe41b4f6c2948c
shreeraam-ramachandran <ramachandran.shr@northeastern.edu>
2023-12-13 20:01:45 -0500

    User login error handeling

--- tasty-trails-client/src/containers/LoginFormContainer.tsx
+++ tasty-trails-client/src/containers/LoginFormContainer.tsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { setAuth } from '../auth/authSlice.ts';
 import LoginForm from '../components/LoginForm/LoginForm.tsx';
 import { loginUser } from '../api/index.js';
+import axios, { AxiosError } from 'axios';
+import { handleAxiosError } from '../service/server-error-handler.ts';
  
  
 const LoginFormContainer: React.FC = () => {
@@ -46,37 +48,44 @@ const LoginFormContainer: React.FC = () => {
  
     return isValid;
   };
+
+ 
  
   const handleLoginSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
- 
-    //setErrorMessage('Invalid credentials. Please try again.');
+  
     if (!validateForm()) {
       setErrorMessage('Please fill in the required fields.');
       return;
     }
- 
+  
     try {
       const response = await loginUser(formData);
- 
+  
       if (response.status === 200) {
- 
         const { userId, token } = {
           userId: response.data.user._id,
           token: response.data.token,
         };
         dispatch(setAuth({ userId, token }));
- 
+  
         navigate('/posts', { state: { userId } });
       } else {
-        setErrorMessage('Invalid credentials. Please try again.');
+        setErrorMessage(response.data.message);
       }
-    } catch (error) {
-      console.error('Error during login:', error);
-      setErrorMessage('An error occurred during login. Please try again.');
+    } catch (error:any) {
+
+      if (error instanceof Error && 'response' in error) {
+        // Handle Axios errors
+        const errorMessage = handleAxiosError(error as AxiosError<ErrorResponse>);
+        setErrorMessage(errorMessage);
+      } else {
+        // Handle non-Axios errors
+        setErrorMessage('An error occurred during login. Please try again.');
+      }
     }
- 
   };
+  
  
   const handleSignupClick = () => {
     navigate('/signup');
